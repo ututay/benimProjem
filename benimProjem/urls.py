fab1b42f9eb2cedd684bc26a5978755db0323878
Ünal TUTAY <tutay@live.com>
2016-10-01 13:42:50 +0300

    URL haritalandırması ve rango index view oluşturuldu

--- benimProjem/urls.py
+++ benimProjem/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from rango import views
+from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r"^$",views.index,name="index"),
+	url(r"^rango/", include("rango.urls")),
 ]
