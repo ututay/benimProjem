73554ff7b820dd40e709c2782607edac91362d42
Ünal TUTAY <tutay@live.com>
2016-10-02 08:27:26 +0300

    Template oluşturulması öğrenildi.

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Merhaba Dünya! Bu bir rango uygulamasıdır!")
+    şablonDeğişkenleri = {"boldmessage":"Gevrek,kremalı,kurabiye,şeker,kek"}
+    return render(request,"rango/index.html",context=şablonDeğişkenleri)
+def about(request):
+    return HttpResponse("""Hakkımızda sayfasına hoşgeldiniz. -> <a href="/rango/" title="Anasayfa">Anasayfa</a>""")
