baa605c6180f2d7217059e461342b1713c0728d7
Ünal TUTAY <tutay@live.com>
2016-10-02 15:52:45 +0300

    Şablonlara about.html adında Hakkımda şablondu eklendi.

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 def index(request):
     şablonDeğişkenleri = {"boldmessage":"Gevrek,kremalı,kurabiye,şeker,kek"}
     return render(request,"rango/index.html",context=şablonDeğişkenleri)
 def about(request):
-    return HttpResponse("""Hakkımızda sayfasına hoşgeldiniz. -> <a href="/rango/" title="Anasayfa">Anasayfa</a>""")
+    şablonDeğişkenleri = {"isimsoyisim":"Ünal TUTAY"}
+    return render(request,"rango/about.html",context=şablonDeğişkenleri)
