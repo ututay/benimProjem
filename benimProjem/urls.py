fbea04058c2c3d9d1a7de79392c4c1be58962879
Ünal TUTAY <tutay@live.com>
2016-10-02 12:08:07 +0300

    Media dosyaları sunmak için gerekli yapılandırmalar yapıldı.

--- benimProjem/urls.py
+++ benimProjem/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from rango import views
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r"^$",views.index,name="index"),
 	url(r"^rango/", include("rango.urls")),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
