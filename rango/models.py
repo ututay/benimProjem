d219546299bd18be1aaaa318f951c6c9e01a956e
Ünal TUTAY <tutay@live.com>
2016-10-04 20:19:11 +0300

    Veritabanı için Model oluşturuldu.

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class Kategori(models.Model):
+    kategoriIsim = models.CharField(max_length=128,unique=True)
+    def str(self):
+        return self.name
+class Sayfa(models.Model):
+    iliski = models.ForeignKey(Kategori)
+    sayfaIsim = models.CharField(max_length=128)
+    sayfaUrl = models.URLField()
+    sayfaGoruntuleme = models.IntegerField(default=0)
+    def str(self):
+        return self.name
