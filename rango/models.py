3d4b9f6ae28279ea5e1b3a0c373fe87b1abbf5c3
Ünal TUTAY <tutay@live.com>
2016-10-05 18:42:50 +0300

    Rango uygulaması Models veritabanı için düzenleme yapıldı.

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 class Kategori(models.Model):
     kategoriIsim = models.CharField(max_length=128,unique=True)
-    def str(self):
-        return self.name
+    def __str__(self):
+        return self.kategoriIsim
 class Sayfa(models.Model):
     iliski = models.ForeignKey(Kategori)
     sayfaIsim = models.CharField(max_length=128)
     sayfaUrl = models.URLField()
     sayfaGoruntuleme = models.IntegerField(default=0)
-    def str(self):
-        return self.name
+    def __str__(self):
+        return self.sayfaIsim
