309165baa00fdc9c47ff543458983dc77fc61044
Ünal TUTAY <tutay@live.com>
2016-10-07 20:20:06 +0300

    Modellerin için çoğul isimlendirme için nested Meta sınıfı eklendi.

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 class Kategori(models.Model):
     kategoriIsim = models.CharField(max_length=128,unique=True)
+    class Meta:
+        verbose_name_plural="Kategoriler"
     def __str__(self):
         return self.kategoriIsim
 class Sayfa(models.Model):
@@ -8,5 +10,7 @@ class Sayfa(models.Model):
     sayfaIsim = models.CharField(max_length=128)
     sayfaUrl = models.URLField()
     sayfaGoruntuleme = models.IntegerField(default=0)
+    class Meta:
+        verbose_name_plural="Sayfalar"
     def __str__(self):
         return self.sayfaIsim
