e65c6c859b7c20ab0cb31eb3e15bf587fc15313b
Ünal TUTAY <tutay@live.com>
2016-10-10 13:39:06 +0300

    En çok beğenilen 5 Kategori anasayfa'ya eklendi.

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+# Kategori model 'i içer aktrılıyor.
+from rango.models import Kategori
+
 def index(request):
-    şablonDeğişkenleri = {"boldmessage":"Gevrek,kremalı,kurabiye,şeker,kek"}
-    return render(request,"rango/index.html",context=şablonDeğişkenleri)
+    kategoriler = Kategori.objects.order_by("-kategoriBegeni")[:5]
+    içerik = {"kategoriler":kategoriler}
+    return render(request,"rango/index.html",context=içerik)
 def about(request):
     şablonDeğişkenleri = {"isimsoyisim":"Ünal TUTAY"}
     return render(request,"rango/about.html",context=şablonDeğişkenleri)
