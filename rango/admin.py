7aa66a7f2d7ca90dda10f25572ae923fa430871f
Ünal TUTAY <tutay@live.com>
2016-10-10 21:56:26 +0300

    Kategori Modeli 'nin yönetici ekranı düzenlendi.
    Slug 'un yazılan kategori ismine göre prepopulated_fields niteliğiyle
    otomatik doldurulması sağlandı. Ayrıca kullanılmayan ama her ne hikmetse
    yazılmış olan SayfaInline sınıfı çıkarıldı.

--- rango/admin.py
+++ rango/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from rango.models import Kategori,Sayfa
-class SayfaInline(admin.StackedInline):
-    model = Sayfa
-    extra = 3
 class SayfaAdmin(admin.ModelAdmin):
     list_display = ("sayfaIsimDef","kategoriDef","sayfaUrlDef")
-admin.site.register(Kategori)
+class KategoriAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"harfDizisi":("kategoriIsim",)}
+admin.site.register(Kategori,KategoriAdmin)
 admin.site.register(Sayfa,SayfaAdmin)
