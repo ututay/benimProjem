5a343e47ed6b48851da8822168947406e099f6f9
Ünal TUTAY <tutay@live.com>
2016-10-13 21:29:44 +0300

    Sayfa Admin panelinde Kategorilere göre filtre etmek için özellik eklendi.

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from rango.models import Kategori,Sayfa
 class SayfaAdmin(admin.ModelAdmin):
     list_display = ("sayfaIsimDef","kategoriDef","sayfaUrlDef")
+    list_filter = ("kategori",)
 class KategoriAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("kategoriIsim",)}
 admin.site.register(Kategori,KategoriAdmin)
