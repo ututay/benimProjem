ccdac9f25cdd176cf0bb3c66ab7f29c83d2db3dd
Ünal TUTAY <tutay@live.com>
2016-10-13 23:08:14 +0300

    Kategori bünyesindeki sayfaların listelenmesi için gerekli kodlamalar yapıldı.

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,6 @@ from rango import views
 
 urlpatterns = [
     url(r"^$",views.index,name="index"),
-    url(r"^about/",views.about,name="about")
+    url(r"^about/",views.about,name="about"),
+    url(r"^kategori/(?P<kategoriAdresi>[\w\-]+)/$",views.sayfalarıGöster,name="sayfalariGoster"),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 # Kategori model 'i içer aktrılıyor.
-from rango.models import Kategori
+from rango.models import Kategori,Sayfa
 
 def index(request):
     kategoriler = Kategori.objects.order_by("-kategoriBegeni")[:5]
     içerik = {"kategoriler":kategoriler}
     return render(request,"rango/index.html",context=içerik)
+
 def about(request):
     şablonDeğişkenleri = {"isimsoyisim":"Ünal TUTAY"}
     return render(request,"rango/about.html",context=şablonDeğişkenleri)
+
+def sayfalarıGöster(request,kategoriAdresi):
+    içerik = dict()
+    try:
+        kategori = Kategori.objects.get(slug=kategoriAdresi)
+        sayfalar = Sayfa.objects.filter(kategori=kategori)
+        içerik["kategori"]=kategori
+        içerik["sayfalar"]=sayfalar
+    except Kategori.DoesNotExist:
+        içerik["kategori"]=None
+        içerik["sayfalar"]=None
+        print("{} adresiyle eşleşen herhangi bir kategori bulunamadı.".format(kategoriAdresi))
+    return render(request,"rango/kategoriler.html",context=içerik)
