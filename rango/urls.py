3a8f9b998849c2eee730ac2a002c597b9a265b8d
Ünal TUTAY <tutay@live.com>
2016-10-16 13:26:47 +0300

    Daha modüler bir yapı için şablonlar içindeki URL 'lara {% url %} etiketi eklendi.
    Ayrıca UrlConf 'lardaki name argümanlarına yönlendirme için değer
    verildi.

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from rango import views
 
+app_name = "rango"
 urlpatterns = [
-    url(r"^$",views.index,name="index"),
-    url(r"^about/",views.about,name="about"),
-    url(r"^kategori/(?P<kategoriAdresi>[\w\-]+)/$",views.sayfalarıGöster,name="sayfalariGoster"),
+    url(r"^$",views.index,name="anasayfa"),
+    url(r"^about/",views.about,name="hakkımda"),
+    url(r"^kategori/(?P<kategoriAdresi>[\w\-]+)/$",views.sayfalarıGöster,name="sayfalarıGöster"),
 ]
