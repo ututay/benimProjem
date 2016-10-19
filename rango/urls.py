9012d14829451525a6080df91195b8e139656fb1
Ünal TUTAY <tutay@live.com>
2016-10-19 10:19:08 +0300

    Kategori ve Sayfa ekleme şablonları için view fonksiyonları oluşturuldu.

--- rango/urls.py
+++ rango/urls.py
@@ -3,7 +3,9 @@ from rango import views
 
 app_name = "rango"
 urlpatterns = [
-    url(r"^$",views.index,name="anasayfa"),
+    url(r"^$",views.anasayfa,name="anasayfa"),
     url(r"^about/",views.about,name="hakkımda"),
     url(r"^kategori/(?P<kategoriAdresi>[\w\-]+)/$",views.sayfalarıGöster,name="sayfalarıGöster"),
+    url(r"^kategori-ekle/$",views.kategoriEkle,name="kategori-ekle"),
+    url(r"^sayfa-ekle/(?P<kategoriAdresi>[\w\-]+)/$",views.sayfaEkle,name="sayfa-ekle"),
 ]
