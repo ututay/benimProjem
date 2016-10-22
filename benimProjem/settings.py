61deba8da52e3f492d6f78ddc856d594359d1f79
Ünal TUTAY <tutay@live.com>
2016-10-22 20:10:29 +0300

    Uygulamalar içindeki template klasörü iptal edildi.

--- benimProjem/settings.py
+++ benimProjem/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATE_DIR,],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
